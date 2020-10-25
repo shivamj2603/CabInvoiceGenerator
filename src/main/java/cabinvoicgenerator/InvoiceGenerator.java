ae498187dd92ce706570a74f00d1cd9d523068eb
shivamj2603 <shivamj2603@gmail.com>
2020-10-25 20:42:11 +0530

    Added:Functionality to get details of total number of rides along with average

--- src/main/java/cabinvoicgenerator/InvoiceGenerator.java
+++ src/main/java/cabinvoicgenerator/InvoiceGenerator.java
@@ -4,6 +4,13 @@ public class InvoiceGenerator {
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
+	/**
+	 * Usecase 1
+	 * Function finds the fare for a ride
+	 * @param distance
+	 * @param time
+	 * @return
+	 */
 	public double calculateFare(double distance, int time) {	
 		double totalfare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 		if(totalfare < MINIMUM_FARE) {
@@ -11,11 +18,18 @@ public class InvoiceGenerator {
 		}
 		return totalfare;
 	}
-	public double calculateFare(Ride[] rides) {
+	/**
+	 * Usecase 2
+	 * Function calculates the fare for multiple ride
+	 * @param rides
+	 * @return
+	 */
+	public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for(Ride ride : rides) {
 			totalFare += this.calculateFare(ride.distance, ride.time);
 		}
-		return totalFare;
+		InvoiceSummary invoiceSummary = new InvoiceSummary(rides.length, totalFare);
+		return invoiceSummary;
 	}
 }
