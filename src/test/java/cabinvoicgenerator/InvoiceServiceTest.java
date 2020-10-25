3da5a4a8eb084f0c07e8a97a3b16ede2cefd9d47
shivamj2603 <shivamj2603@gmail.com>
2020-10-25 22:38:08 +0530

    Added:Test Case to calculate fare for different type of rides

--- src/test/java/cabinvoicgenerator/InvoiceServiceTest.java
+++ src/test/java/cabinvoicgenerator/InvoiceServiceTest.java
@@ -8,7 +8,7 @@ public class InvoiceServiceTest {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 3.0;
 		int time = 5;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		double fare = invoiceGenerator.calculateFare(distance, time, "Normal");
 		assertEquals(35, fare, 0.0);
 	}
 	@Test
@@ -16,18 +16,26 @@ public class InvoiceServiceTest {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 2;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		double fare = invoiceGenerator.calculateFare(distance, time, "Normal");
 		assertEquals(5, fare, 0.0);
 	}
 	@Test
 	public void givenUserId_getListOfRides_shouldReturnInvoiceSummary() {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-		Ride[] rides = { new Ride(1, 3.0, 5),
-		         new Ride(1, 0.1, 2)
+		Ride[] rides = { new Ride(1, 3.0, 5, "Normal"),
+		         new Ride(1, 0.1, 2, "Normal")
               };
 		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 40);
-		assertEquals(expectedInvoiceSummary, invoiceGenerator.calculateFare(rides, 1));
-		
+		assertEquals(expectedInvoiceSummary, invoiceGenerator.calculateFare(rides, 1));		
+	}
+	@Test
+	public void givenPremiumRide_getListOfRides_shouldReturnInvoiceSummary() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(1, 3.0, 5, "Premium"),
+		         new Ride(1, 0.1, 2, "Premium")
+              };
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 75);
+		assertEquals(expectedInvoiceSummary, invoiceGenerator.calculateFare(rides, 1));		
 	}
 	
 }
