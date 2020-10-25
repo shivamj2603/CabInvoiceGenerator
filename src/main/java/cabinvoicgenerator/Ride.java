2859266f6468626ba3840fa6420ba2a89c204073
shivamj2603 <shivamj2603@gmail.com>
2020-10-25 22:37:24 +0530

    Added:Field Member typeOfRide

--- src/main/java/cabinvoicgenerator/Ride.java
+++ src/main/java/cabinvoicgenerator/Ride.java
@@ -4,9 +4,11 @@ public class Ride {
 	public int userId;
 	public double distance;
 	public int time;
-	public Ride(int userId, double distance , int time) {
+	public String typeOfRide;
+	public Ride(int userId, double distance , int time, String typeOfRide) {
 		this.userId = userId;
 		this.distance = distance;
 		this.time = time;
+		this.typeOfRide = typeOfRide;
 	}
 }
