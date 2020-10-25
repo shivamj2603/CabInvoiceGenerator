7f5c6f964818e92088d3e751a3950814915ed341
shivamj2603 <shivamj2603@gmail.com>
2020-10-25 22:02:14 +0530

    Added:UserId as class member

--- src/main/java/cabinvoicgenerator/Ride.java
+++ src/main/java/cabinvoicgenerator/Ride.java
@@ -1,9 +1,11 @@
 package cabinvoicgenerator;
 
 public class Ride {
+	public int userId;
 	public double distance;
 	public int time;
-	public Ride(double distance , int time) {
+	public Ride(int userId, double distance , int time) {
+		this.userId = userId;
 		this.distance = distance;
 		this.time = time;
 	}
