43be504efb8e4167e32d026f0278915dd257928c
shivamj2603 <shivamj2603@gmail.com>
2020-10-25 20:20:49 +0530

    Tested:Functionality for calculating total fare for multiple rides

--- src/test/java/cabinvoicgenerator/InvoiceServiceTest.java
+++ src/test/java/cabinvoicgenerator/InvoiceServiceTest.java
@@ -19,4 +19,12 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(5, fare, 0.0);
 	}
+	@Test
+	public void givenMultipleRides_shouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(3.0, 5),
+				         new Ride(0.1, 2)
+		               };
+		assertEquals(40, invoiceGenerator.calculateFare(rides), 0.0);	
+	}
 }
