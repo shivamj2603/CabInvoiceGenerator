7e8d1957b2b603288fa19e1e6c0b8e34096aacf6
shivamj2603 <shivamj2603@gmail.com>
2020-10-27 07:57:07 +0530

    Added:Test case to check for invoice summary for a given user id

--- src/test/java/cabinvoicgenerator/InvoiceServiceTest.java
+++ src/test/java/cabinvoicgenerator/InvoiceServiceTest.java
@@ -13,30 +13,36 @@ public class InvoiceServiceTest {
 	public void givenDistanceAndTime_shouldReturnTotalFare() {
 		double distance = 3.0;
 		int time = 5;
-		double fare = invoiceGenerator.calculateFare(distance, time, "Normal");
+		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(35, fare, 0.0);
 	}
 	@Test
 	public void givenLessDistanceOrTime_shouldReturnMinFare() {
 		double distance = 0.1;
 		int time = 2;
-		double fare = invoiceGenerator.calculateFare(distance, time, "Normal");
+		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(5, fare, 0.0);
 	}
 	@Test
-	public void givenUserId_getListOfRides_shouldReturnInvoiceSummary() {
-		Ride[] rides = { new Ride(1, 3.0, 5, "Normal"),
-		         new Ride(1, 0.1, 2, "Normal")
-              };
-		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 40);
-		assertEquals(expectedInvoiceSummary, invoiceGenerator.calculateFare(rides, 1));		
+	public void givenMultipleRides_shouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(3.0, 5),
+				         new Ride(0.1, 2)
+		               };
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary excpectedInvoiceSummary = new InvoiceSummary(2, 40.0);
+		assertEquals(excpectedInvoiceSummary,invoiceSummary);	
 	}
 	@Test
-	public void givenPremiumRide_getListOfRides_shouldReturnInvoiceSummary() {
-		Ride[] rides = { new Ride(1, 3.0, 5, "Premium"),
-		         new Ride(1, 0.1, 2, "Premium")
-              };
-		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 75);
-		assertEquals(expectedInvoiceSummary, invoiceGenerator.calculateFare(rides, 1));		
+	public void givenUserIDAndRides_shouldReturn_InvoiceSummary() {
+		String userId = "Shivam";
+		Ride[] rides = { new Ride(3.0, 5),
+		                 new Ride(0.1, 2)
+                       };
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		invoiceGenerator.addRides(userId, rides);
+		InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 40.0);
+		assertEquals(expectedInvoiceSummary, summary);
 	}
 }
