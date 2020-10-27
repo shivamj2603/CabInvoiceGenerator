edd2056106ea8635c38bd317e5ccef6ad5034574
shivamj2603 <shivamj2603@gmail.com>
2020-10-27 07:58:34 +0530

    Refactored:construct logic

--- src/main/java/cabinvoicgenerator/Ride.java
+++ src/main/java/cabinvoicgenerator/Ride.java
@@ -1,14 +1,10 @@
 package cabinvoicgenerator;
 
 public class Ride {
-	public int userId;
 	public double distance;
 	public int time;
-	public String typeOfRide;
-	public Ride(int userId, double distance , int time, String typeOfRide) {
-		this.userId = userId;
+	public Ride(double distance , int time) {
 		this.distance = distance;
 		this.time = time;
-		this.typeOfRide = typeOfRide;
 	}
 }
