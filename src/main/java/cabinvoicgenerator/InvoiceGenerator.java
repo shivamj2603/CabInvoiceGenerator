6ffd0f75a6d915a927894840e5701b1890162945
shivamj2603 <shivamj2603@gmail.com>
2020-10-27 07:59:09 +0530

    Refactored:Construct logic

--- src/main/java/cabinvoicgenerator/InvoiceGenerator.java
+++ src/main/java/cabinvoicgenerator/InvoiceGenerator.java
@@ -4,6 +4,10 @@ public class InvoiceGenerator {
 	private double MINIMUM_COST_PER_KILOMETER = 10;
 	private int COST_PER_TIME = 1;
 	private double MINIMUM_FARE = 5;
+	private RideRepository rideRepository;
+	public InvoiceGenerator() {
+		rideRepository = new RideRepository();
+	}
 	/**
 	 * Usecase 1
 	 * Function finds the fare for a ride
@@ -11,12 +15,7 @@ public class InvoiceGenerator {
 	 * @param time
 	 * @return
 	 */
-	public double calculateFare(double distance, int time, String typeOfRide) {	
-		if(typeOfRide.equalsIgnoreCase("Premium")) {
-			MINIMUM_COST_PER_KILOMETER = 15;
-			COST_PER_TIME = 2;
-			MINIMUM_FARE = 20;
-		}
+	public double calculateFare(double distance, int time) {	
 		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 		if(totalFare < MINIMUM_FARE) {
 			return MINIMUM_FARE;
@@ -29,14 +28,28 @@ public class InvoiceGenerator {
 	 * @param rides
 	 * @return
 	 */
-	public InvoiceSummary calculateFare(Ride[] rides, int userId) {
+	public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for(Ride ride : rides) {
-			if(ride.userId == userId) {
-			totalFare += this.calculateFare(ride.distance, ride.time, ride.typeOfRide);
-			}
+			totalFare += this.calculateFare(ride.distance, ride.time);
 		}
 		InvoiceSummary invoiceSummary = new InvoiceSummary(rides.length, totalFare);
 		return invoiceSummary;
+	}
+	/**
+	 * @param userId
+	 * @param rides
+	 */
+	public void addRides(String userId, Ride[] rides) {
+		rideRepository.addRides(userId, rides);
+	}
+	/**
+	 * Usecase 4
+	 * Get Invoice Summary for given user ID
+	 * @param userId
+	 * @return
+	 */
+	public InvoiceSummary getInvoiceSummary(String userId) {
+		return this.calculateFare(rideRepository.getRides(userId));
 	}
 }
